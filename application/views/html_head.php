dc8f5b5f36f9838ab6d0073f38207851c22e6685
Philip J Schanely <philschanely@cedarville.edu>
2016-06-06 07:15:09 -0400

    update: improve ui; add icons

--- application/views/html_head.php
+++ application/views/html_head.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>{page_title}</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <link type="text/css" rel="stylesheet" href="{base_url}assets/base.css" />
 </head>
 <body ng-app="campfire-client" clas="container">
