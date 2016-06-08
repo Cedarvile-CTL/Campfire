84871426eb4728cf1e6e2c5888dcb657c527caae
Philip J Schanely <philschanely@cedarville.edu>
2016-06-08 07:04:11 -0400

    new: install tinyMCE

--- application/views/html_foot.php
+++ application/views/html_foot.php
@@ -1,4 +1,5 @@
         <script type="text/javascript" src="{base_url}assets/vendor.js"></script>
+        <script type="text/javascript" src="{base_url}assets/tinymce/tinymce.min.js"></script>
         <script type="text/javascript" src="{base_url}assets/app.js"></script>
     </body>
 </html>
