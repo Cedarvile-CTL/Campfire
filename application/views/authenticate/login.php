830c6d48093403bf1c85649ab8a3a203879d55f3
Philip J Schanely <philschanely@cedarville.edu>
2016-06-11 14:00:50 -0400

    update: Spice up UI for login

--- application/views/authenticate/login.php
+++ application/views/authenticate/login.php
@@ -1,23 +1,27 @@
-<h2>Log in</h2>
-<?php if (isset($login_error) && $login_error) : ?>
-<p class="alert alert-warning">{message}</p>
-<?php else : ?>
-<p class="alert alert-warning">You must log in to use this application. Enter your Cedarville credentials below.</p>
-<?php endif; ?>
-<form action="{base_url}authenticate/login" method="post">
-    <ul>
-        <li class="control-group">
-            <label class="control-label">Username</label>
-            <div class="controls">
-                <input type="text" name="username" />
-            </div>
-        </li>
-        <li class="control-group">
-            <label class="control-label">Password</label>
-            <div class="controls">
-                <input type="password" name="password" />
-            </div>
-        </li>
-    </ul>
-    <p><input type="submit" value="Sign in" /></p>
-</form>
+<div class="container">
+    <h2>Log in</h2>
+    <div class="card-panel teal">
+    <?php if (isset($login_error) && $login_error) : ?>
+        <p class="white-text">{message}</p>
+    <?php else : ?>
+        <p class="white-text">You must log in to use this application. Enter your Cedarville University credentials below.</p>
+    <?php endif; ?>
+    </div>
+    <form action="{base_url}authenticate/login" method="post">
+        <ul>
+            <li class="row">
+                <div class="input-field col s12">
+                    <input id="username" name="username" type="text" class="validate">
+                    <label for="username">Cedarville username</label>
+                </div>
+            </li>
+            <li class="row">
+                <div class="input-field col s12">
+                    <input id="password" name="password" type="password" class="validate">
+                    <label for="password">Cedarville password</label>
+                </div>
+            </li>
+        </ul>
+        <p><button type="submit" class="waves-effect waves-light btn">Sign in</button></p>
+    </form>
+</div>
